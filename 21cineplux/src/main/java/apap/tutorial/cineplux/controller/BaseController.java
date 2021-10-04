cca71601030cfdf45df8189918e8c9072c651d4c
unknown <farizhabibiepermana@gmail.com>
2021-10-04 11:37:13 +0700

    feat: add footer fragment

--- 21cineplux/src/main/java/apap/tutorial/cineplux/controller/BaseController.java
+++ 21cineplux/src/main/java/apap/tutorial/cineplux/controller/BaseController.java
@@ -1,10 +1,21 @@
 package apap.tutorial.cineplux.controller;
+import apap.tutorial.cineplux.model.BioskopModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.time.LocalTime;
+import java.util.Arrays;
+
 @Controller
 public class BaseController {
     @GetMapping("/")
-    private String home() {
-        return "home" ;
-    }
+    private String home(
+        @ModelAttribute BioskopModel bioskop,
+        Model model
+    ) {
+            model.addAttribute("time", LocalTime.now());
+            return "home";
+        }
 }
